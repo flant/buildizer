a7bb470ca30e1f167472c6f9ff72e5916fd28250
Timofey Kirillov <distorhead@gmail.com>
2016-03-29 19:44:12 +0300

    fpm_depends support

--- lib/buildizer/target/fpm.rb
+++ lib/buildizer/target/fpm.rb
@@ -5,16 +5,19 @@ module Buildizer
       attr_reader :fpm_config_files
       attr_reader :fpm_files
       attr_reader :fpm_conflicts
+      attr_reader :fpm_depends
 
       def initialize(builder, image,
                      fpm_script: [], fpm_config_files: {},
-                     fpm_files: {}, fpm_conflicts: [], **kwargs)
+                     fpm_files: {}, fpm_conflicts: [],
+                     fpm_depends: [], **kwargs)
         super(builder, image, **kwargs)
 
         @fpm_script = fpm_script
         @fpm_config_files = fpm_config_files
         @fpm_files = fpm_files
         @fpm_conflicts = fpm_conflicts
+        @fpm_depends = fpm_depends
       end
     end # Fpm
   end # Target
