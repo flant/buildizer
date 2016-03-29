476b4cd101dd03d5dcce3db889b4f9886e550f58
Timofey Kirillov <distorhead@gmail.com>
2016-03-29 12:18:08 +0300

    Native builder type fix: do not redefine package_version.

--- lib/buildizer/builder/native.rb
+++ lib/buildizer/builder/native.rb
@@ -9,12 +9,6 @@ module Buildizer
         Target::Native
       end
 
-      def initial_target_params
-        super.tap do |params|
-          params[:package_version] = packager.package_version_tag
-        end
-      end
-
       def build_instructions(target)
         target.image.native_build_instructions(self, target)
       end
